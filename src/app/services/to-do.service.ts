268e08646661d8d8c523fe23847ca3e28c5b70c3
Hermes Soto <hermessoto@192.168.68.100>
2024-10-29 14:02:53 -0400

    initial setup

---
+++ src/app/services/to-do.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToDoService {
+
+}
